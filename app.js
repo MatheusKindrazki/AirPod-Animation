aed16c1a23a25c5b7b9fc3f943297f8d499f7a82
Matheus Kindrazki <matheuskindrazki@gmail.com>
2020-02-22 19:15:26 -0300

    alterando video | Play simplificado

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const controller = new ScrollMagic.Controller();
 
 //Scenes
 let scene = new ScrollMagic.Scene({
-  duration: 9000,
+  duration: 3500,
   triggerElement: intro,
   triggerHook: 0
 })
